b5706f0c4ed6fc3f582d44fa89fed165e5ebca1a
timpelser <timpelser@hotmail.fr>
2016-11-30 20:06:06 +0100

    Changed Constants and added class engine with get and set methods

--- src/main/java/edu/tudelft/games/f1manager/Constants.java
+++ src/main/java/edu/tudelft/games/f1manager/Constants.java
@@ -3,6 +3,8 @@ package edu.tudelft.games.f1manager;
 
 public class Constants {
 
-  public double FUEL_EFFICIENCY = 1.00;
-
+  public double FUELEFFICIENCYCOEF = 1.00;
+  private double POWERCOEF = 1.3;
+  private double DRIVABILITYCOEF = 1.1;
+  private int VALUEENGINE = 10000;
 }
