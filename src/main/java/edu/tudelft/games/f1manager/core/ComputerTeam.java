3feb6b68fc9cf58058068bf966e6a1e7dc94cc52
Elias <elarb@users.noreply.github.com>
2016-12-01 21:03:32 +0100

    Added constructor

--- src/main/java/edu/tudelft/games/f1manager/core/ComputerTeam.java
+++ src/main/java/edu/tudelft/games/f1manager/core/ComputerTeam.java
@@ -2,15 +2,23 @@ package edu.tudelft.games.f1manager.core;
 
 
 /**
- * class that represtents a Computer team
+ * class that represents a Computer team.
  */
 public class ComputerTeam extends Team {
 
+
   /**
-   * Constuctor for ComputerTeam
+   * Creates an Object that represents a F1 Team.
+   *
+   * @param driver1        Driver #1 of the team
+   * @param driver2        Driver #2 of the team
+   * @param car1           Car #1 of the team
+   * @param car2           Car #2 of the team
+   * @param strategist     Strategist of the team
+   * @param aerodynamicist Aerodynamicist of the team
+   * @param mechanic       Mechanic of the team
    */
-  public ComputerTeam(){
-
+  public ComputerTeam(Driver driver1, Driver driver2, Car car1, Car car2, Strategist strategist, Aerodynamicist aerodynamicist, Mechanic mechanic) {
+    super(driver1, driver2, car1, car2, strategist, aerodynamicist, mechanic);
   }
-
 }
