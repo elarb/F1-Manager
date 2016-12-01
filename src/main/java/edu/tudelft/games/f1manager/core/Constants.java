a791f207fb9b76da75fce81e0f9cb3bf79977655
timpelser <timpelser@hotmail.fr>
2016-12-01 15:12:37 +0100

    Changed file position

--- src/main/java/edu/tudelft/games/f1manager/core/Constants.java
+++ src/main/java/edu/tudelft/games/f1manager/core/Constants.java
@@ -1,12 +1,10 @@
 package edu.tudelft.games.f1manager.core;
 
-/**
- * This class should contain all constants (numbers, decimals, strings, etc.)
- */
+
 public class Constants {
 
-  public static double FUELEFFICIENCYCOEF = 1.00;
-  public static double POWERCOEF = 1.3;
-  public static double DRIVABILITYCOEF = 1.1;
-  public static int VALUEENGINE = 10000;
+  public static double FUELEFFICIENCYCOEF;
+  public static double POWERCOEF;
+  public static double DRIVABILITYCOEF;
+  public static int VALUEENGINE;
 }
--- src/main/java/edu/tudelft/games/f1manager/core/Engine.java
+++ src/main/java/edu/tudelft/games/f1manager/core/Engine.java
@@ -1,71 +1,61 @@
 package edu.tudelft.games.f1manager.core;
 
-/**
- * This class represents an engine of a F1 car.
- */
 public class Engine {
 
   private String brand;
   private double power;
-  private double driveAbility;
-  private double fuelEfficiency;
-  private int price;
+  private double drivability;
+  private double fuelefficiency;
+  private double priceengine;
 
-  /**
-   * Create an Object that represents an Engine.
-   *
-   * @param brand          The brand of the Engine
-   * @param power          Power of the Engine
-   * @param driveAbility   The Drive-ability of the Engine
-   * @param fuelEfficiency The Fuel-efficienty of the Engine
-   */
-  public Engine(String brand, double power, double driveAbility, double fuelEfficiency, int price) {
+
+
+  public Engine(String brand, double power, double drivability, double fuelefficiency){
     this.brand = brand;
     this.power = power;
-    this.driveAbility = driveAbility;
-    this.fuelEfficiency = fuelEfficiency;
-    this.price = price;
+    this.drivability = drivability;
+    this.fuelefficiency = fuelefficiency;
   }
 
-  public String getBrand() {
+  public String getBrand(){
     return brand;
   }
 
-  public void setBrand(String brand) {
-    this.brand = brand;
+  public void setBrand(String newbrand){
+    brand = newbrand;
   }
 
-  public double getPower() {
+  public double getPrice(){
+    return priceengine;
+  }
+
+  public double getPower(){
     return power;
   }
 
-  public void setPower(double power) {
-    this.power = power;
+  public void setPower(double newpower){
+    power = newpower;
   }
 
-  public double getDriveAbility() {
-    return driveAbility;
+  public double getDrivability(){
+    return drivability;
   }
 
-  public void setDriveAbility(double driveAbility) {
-    this.driveAbility = driveAbility;
+  public void setDrivability(double newdrivability){
+    drivability = newdrivability;
   }
 
-  public double getFuelEfficiency() {
-    return fuelEfficiency;
+  public double getFuelefficiency(){
+    return fuelefficiency;
   }
 
-  public void setFuelEfficiency(double fuelEfficiency) {
-    this.fuelEfficiency = fuelEfficiency;
+  public void setPrice() {
+    double calculus = Constants.VALUEENGINE*((Constants.POWERCOEF*power)*(Constants.DRIVABILITYCOEF*drivability)*(Constants.FUELEFFICIENCYCOEF*fuelefficiency));
+    priceengine = calculus;
   }
 
-  public int getPrice() {
-    return price;
-  }
 
-  public void setPrice(int price) {
-    this.price = price;
-  }
+
 }
 
 
