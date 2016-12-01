172c675d138341bb1a0e66c20b36001d4ac7bfbd
timpelser <timpelser@hotmail.fr>
2016-12-01 15:05:36 +0100

    Added Method for calculating price

--- src/main/java/edu/tudelft/games/f1manager/Constants.java
+++ src/main/java/edu/tudelft/games/f1manager/Constants.java
@@ -3,8 +3,8 @@ package edu.tudelft.games.f1manager;
 
 public class Constants {
 
-  public double FUELEFFICIENCYCOEF = 1.00;
-  private double POWERCOEF = 1.3;
-  private double DRIVABILITYCOEF = 1.1;
-  private int VALUEENGINE = 10000;
+  public static double FUELEFFICIENCYCOEF;
+  public static double POWERCOEF;
+  public static double DRIVABILITYCOEF;
+  public static int VALUEENGINE;
 }
