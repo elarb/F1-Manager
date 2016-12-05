feabc37ff1248ae762cace860f50758462a9dc9a
Wouter <Wouter_van_Lil@live.nl>
2016-12-05 13:35:21 +0100

    `Driver & Card

--- src/main/java/edu/tudelft/games/f1manager/core/Driver.java
+++ src/main/java/edu/tudelft/games/f1manager/core/Driver.java
@@ -1,5 +1,18 @@
 package edu.tudelft.games.f1manager.core;
 
-
 public class Driver {
+	
+	private String name;
+	private Team team;
+	
+	private double speed;
+	private double racecraft;
+	private double strategyinsight;
+	private int value;
+	
+	public Driver(String iname, Team iteam) {
+		this.name = iname;
+		this.team = iteam;
+	}
+	
 }
