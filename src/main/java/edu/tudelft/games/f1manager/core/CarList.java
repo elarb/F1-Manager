da51beb1a7f5f508880de0c08e20e63050b08249
Elias <elarb@users.noreply.github.com>
2016-12-07 22:54:02 +0100

    AddCar method in CarList

--- src/main/java/edu/tudelft/games/f1manager/core/CarList.java
+++ src/main/java/edu/tudelft/games/f1manager/core/CarList.java
@@ -2,20 +2,25 @@ package edu.tudelft.games.f1manager.core;
 
 import java.util.ArrayList;
 
-public class CarList{
+public class CarList {
 
-private ArrayList<Car> carList;
+  private ArrayList<Car> carList;
 
+  public void addCar(Car car) {
 
-public CarList(ArrayList<Car> carList) {
-  this.carList = carList;
+    this.carList.add(car);
+
   }
 
-public ArrayList<Car> getcarList() {
-  return carList;
+  public CarList(ArrayList<Car> carList) {
+    this.carList = carList;
   }
 
-public void setcarList(ArrayList<Car> carList) {
-  this.carList = carList;
+  public ArrayList<Car> getcarList() {
+    return carList;
+  }
+
+  public void setcarList(ArrayList<Car> carList) {
+    this.carList = carList;
   }
 }
