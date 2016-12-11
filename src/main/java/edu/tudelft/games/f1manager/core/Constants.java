6f4d15eb385129028fa986a16945a940db55a011
e_lar <elarb@users.noreply.github.com>
2016-12-11 13:10:37 +0100

    Added constant "Crash Chance"

--- src/main/java/edu/tudelft/games/f1manager/core/Constants.java
+++ src/main/java/edu/tudelft/games/f1manager/core/Constants.java
@@ -3,14 +3,16 @@ package edu.tudelft.games.f1manager.core;
 
 public class Constants {
 
-  public static double FUEL_EFFICIENCYCOEF;
-  public static double POWER_COEF;
-  public static double DRIVABILITY_COEF;
-  public static int VALUE_ENGINE;
-  public static int SPEEDCOEF;
-  public static int RACECRAFTCOEF;
-  public static int STRATEGYINSIGHTCOEF;
-  public static int DRIVERBASEPRICE;
-  public static int BASE_PITSTOP_UP_PRICE = 2000000;
+  public static final double FUEL_EFFICIENCYCOEF = 0;
+  public static final double POWER_COEF = 0;
+  public static final double DRIVABILITY_COEF = 0;
+  public static final int VALUE_ENGINE = 0;
+  public static final int SPEEDCOEF = 0;
+  public static final int RACECRAFTCOEF = 0;
+  public static final int STRATEGYINSIGHTCOEF = 0;
+  public static final int DRIVERBASEPRICE = 0;
+  public static final int BASE_PITSTOP_UP_PRICE = 2000000;
+
+  public static final int CRASH_CHANCE = 8;
 
 }
