9cb46c1f548799bd8dd60eeea36cb5a92c1ff940
timpelser <timpelser@hotmail.fr>
2016-12-12 16:22:22 +0100

    Added Enginelist (For easier graphic interface)

---
+++ src/main/java/edu/tudelft/games/f1manager/core/EngineList.java
@@ -0,0 +1,24 @@
+package edu.tudelft.games.f1manager.core;
+
+import java.util.ArrayList;
+
+public class EngineList {
+
+  ArrayList<Engine> enginelist;
+
+  public EngineList (ArrayList<Engine> enginelist) {
+    enginelist = this.enginelist;
+  }
+
+  public void addEngine (Engine engine) {
+    enginelist.add(engine);
+  }
+
+  public ArrayList<Engine> getEnginelist() {
+    return enginelist;
+  }
+
+  public void setEnginelist(ArrayList<Engine> enginelist) {
+    this.enginelist = enginelist;
+  }
+}
