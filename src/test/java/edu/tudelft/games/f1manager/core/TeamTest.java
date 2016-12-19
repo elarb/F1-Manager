35d7e9c2eaa5e9606e492d0336a5519c00288b7e
David-Happel <davidvhappel1@gmail.com>
2016-12-19 14:04:22 +0100

    equals for team, no working tests yet

--- src/test/java/edu/tudelft/games/f1manager/core/TeamTest.java
+++ src/test/java/edu/tudelft/games/f1manager/core/TeamTest.java
@@ -1,20 +1,49 @@
 package edu.tudelft.games.f1manager.core;
 
-import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
-/**
- * Created by Elias on 30-Nov-16.
- */
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+
 public class TeamTest {
+
+  private Team playerTeam;
+  private Team playerTeam2;
+
   @Before
+  //TODO: mock doesn't work for equals
   public void setUp() throws Exception {
+    playerTeam = new PlayerTeam(new ArrayList<Driver>(),new ArrayList<Car>(),
+      mock(Strategist.class), mock(Aerodynamicist.class), mock(Mechanic.class),
+      200, true);
+
+    playerTeam2 = playerTeam;
+
+
+
 
   }
 
-  @After
-  public void tearDown() throws Exception {
+  @Test
+  public void testEquals_notEqual(){
+    playerTeam.getDriverList().add(new Driver("test testson", playerTeam));
+    assertFalse(playerTeam.equals(playerTeam2));
+  }
 
+  @Test
+  public void testEquals_EqualSame(){
+    assertTrue(playerTeam.equals(playerTeam));
+  }
+
+
+  @Test
+  public void testEquals_EqualDifferent(){
+    assertTrue(playerTeam.equals(playerTeam2));
   }
 
 }
