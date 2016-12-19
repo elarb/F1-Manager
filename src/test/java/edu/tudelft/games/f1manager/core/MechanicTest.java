1826d1741edf97a20e51b9adf07ffc8cd7683e27
Wouter <Wouter_van_Lil@live.nl>
2016-12-19 15:49:18 +0100

    Both scenarios

--- src/test/java/edu/tudelft/games/f1manager/core/MechanicTest.java
+++ src/test/java/edu/tudelft/games/f1manager/core/MechanicTest.java
@@ -34,5 +34,17 @@ public class MechanicTest {
 	  assertEquals("Improve works properly", mechanic.getPitstopTime(), 7);
 	  assertEquals("Improve adjusts cost", mechanic.getUpgradePrice(), Constants.BASE_PITSTOP_UP_PRICE *2);
   }
+  
+  @Test
+  public void testImproveFail() throws Exception {
+	  
+	  PlayerTeam playerTeam = new PlayerTeam(new ArrayList<Driver>(),new ArrayList<Car>(),
+		      mock(Strategist.class), mock(Aerodynamicist.class), mock(Mechanic.class),
+		      1999999, true);
+	  mechanic.improve(playerTeam);
+	  mechanic.updateUpgradePrice();
+	  assertEquals("Improve didn't do anything", mechanic.getPitstopTime(), 8);
+	  assertEquals("Costs the same", mechanic.getUpgradePrice(), Constants.BASE_PITSTOP_UP_PRICE);
+  }
 
 }
