f1e27361344e47eadc388f6a8ae69fecfa028a1f
timpelser <timpelser@hotmail.fr>
2016-12-22 14:43:47 +0100

    Added GetJSON/UpdateJSON to PlayerTeam and Season
    Added Read to Season

--- src/main/java/edu/tudelft/games/f1manager/game/Season.java
+++ src/main/java/edu/tudelft/games/f1manager/game/Season.java
@@ -1,7 +1,10 @@
 package edu.tudelft.games.f1manager.game;
 
+import com.google.gson.Gson;
+import edu.tudelft.games.f1manager.core.PlayerTeam;
 import edu.tudelft.games.f1manager.core.Team;
 
+import java.io.*;
 import java.util.ArrayList;
 
 
@@ -11,6 +14,7 @@ public class Season {
    * The current race in the season.
    */
   private int currentRace;
+  private Gson gson = new Gson();
 
   /**
    * List of drivers sorted by points.
@@ -43,4 +47,42 @@ public class Season {
   public void setConstructorStandings(ArrayList<Team> constructorStandings) {
     this.constructorStandings = constructorStandings;
   }
+
+  public Season read() {
+
+    String fileName = "season.json";
+
+    ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+    InputStream is = classloader.getResourceAsStream(fileName);
+    Reader reader = new InputStreamReader(is);
+    Season season = gson.fromJson(reader, Season.class);
+
+    return season;
+
+  }
+
+  public void getJSON() {
+
+    Season newseason = read();
+    this.constructorStandings = newseason.getConstructorStandings();
+    this.currentRace = newseason.getCurrentRace();
+
+
+  }
+
+
+  public void updateJSON() throws IOException {
+
+    String fileName = "season.json";
+
+    Season season = new Season(this.getCurrentRace(), this.getConstructorStandings());
+
+    String json = gson.toJson(season);
+
+    FileOutputStream outputStream = new FileOutputStream("src/main/resources/" + fileName);
+    outputStream.write(json.getBytes());
+    outputStream.close();
+
+  }
+
 }
