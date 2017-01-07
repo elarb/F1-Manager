de3db3e3219e1c7a19591625300763e544de9bc4
e_lar <elarb@users.noreply.github.com>
2017-01-07 18:14:16 +0100

    added teamid field

--- src/main/java/edu/tudelft/games/f1manager/core/Driver.java
+++ src/main/java/edu/tudelft/games/f1manager/core/Driver.java
@@ -1,80 +1,75 @@
 package edu.tudelft.games.f1manager.core;
 
 public class Driver {
-	
-	private String name;
-	private Team team;
-	
-	private double speed;
-	private double racecraft;
-	private double strategyinsight;
-	private int value;
-	
-	public Driver(String iname, Team iteam) {
-		this.name = iname;
-		this.team = iteam;
-	}
+
+  private String name;
+  private double speed;
+  private double racecraft;
+  private double strategyinsight;
+  private int value;
 
   /**
-   * removes the player from the current Team and ads it to team.
-   * changes its over Team to team
-   * @param team - Team
+   * Id of the team associated to the driver.
    */
-	public void transfer(Team team){
-    this.getTeam().getDriverList().remove(this);
-    this.setTeam(team);
-    team.getDriverList().add(this);
+  private int teamId;
+
+  /**
+   * Creates an object that represents a F1 Driver.
+   *
+   * @param name   name of the driver
+   * @param teamId if of the team associated to the driver
+   */
+  public Driver(String name, int teamId) {
+    this.name = name;
+    this.teamId = teamId;
   }
-	
-	public void calcValue() {
-		this.value = 123; // formula
-	}
 
-	public String getName() {
-		return name;
-	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public Team getTeam() {
-		return team;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public void setTeam(Team team) {
-		this.team = team;
-	}
+  public double getSpeed() {
+    return speed;
+  }
 
-	public double getSpeed() {
-		return speed;
-	}
+  public void setSpeed(double speed) {
+    this.speed = speed;
+  }
 
-	public void setSpeed(double speed) {
-		this.speed = speed;
-	}
+  public double getRacecraft() {
+    return racecraft;
+  }
 
-	public double getRacecraft() {
-		return racecraft;
-	}
+  public void setRacecraft(double racecraft) {
+    this.racecraft = racecraft;
+  }
 
-	public void setRacecraft(double racecraft) {
-		this.racecraft = racecraft;
-	}
+  public double getStrategyinsight() {
+    return strategyinsight;
+  }
 
-	public double getStrategyinsight() {
-		return strategyinsight;
-	}
+  public void setStrategyinsight(double strategyinsight) {
+    this.strategyinsight = strategyinsight;
+  }
 
-	public void setStrategyinsight(double strategyinsight) {
-		this.strategyinsight = strategyinsight;
-	}
-	
-	public int getValue() {
-		return value;
-	}
+  public int getValue() {
+    return value;
+  }
 
   public void setValue(int value) {
     this.value = value;
+  }
+
+  public int getTeamId() {
+    return teamId;
+  }
+
+  public void setTeamId(int teamId) {
+    this.teamId = teamId;
   }
 }
