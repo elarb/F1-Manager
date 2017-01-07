8cd93182c3967bd983ea801f9f3305f3e8fe8a97
timpelser <timpelser@hotmail.fr>
2017-01-07 15:01:55 +0100

    Changes to getJSOn
    Working:
    
    -Season
    
    In Progress (problem with Team class (Abstract)) :
    
    -Playerteam
    -Driverslist
    -AiTeamlist

--- src/main/java/edu/tudelft/games/f1manager/game/Season.java
+++ src/main/java/edu/tudelft/games/f1manager/game/Season.java
@@ -1,10 +1,12 @@
 package edu.tudelft.games.f1manager.game;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import edu.tudelft.games.f1manager.core.PlayerTeam;
 import edu.tudelft.games.f1manager.core.Team;
 
 import java.io.*;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 
 
@@ -14,7 +16,10 @@ public class Season {
    * The current race in the season.
    */
   private int currentRace;
-  private Gson gson = new Gson();
+  private Gson gson = new GsonBuilder()
+    .excludeFieldsWithModifiers(Modifier.FINAL, Modifier.TRANSIENT, Modifier.STATIC)
+    .serializeNulls()
+    .create();
 
   /**
    * List of drivers sorted by points.
@@ -53,7 +58,7 @@ public class Season {
     String fileName = "season.json";
 
     ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-    InputStream is = classloader.getResourceAsStream(fileName);
+    InputStream is = classloader.getResourceAsStream("JSON/" + fileName);
     Reader reader = new InputStreamReader(is);
     Season season = gson.fromJson(reader, Season.class);
 
@@ -79,7 +84,7 @@ public class Season {
 
     String json = gson.toJson(season);
 
-    FileOutputStream outputStream = new FileOutputStream("src/main/resources/JSON" + fileName);
+    FileOutputStream outputStream = new FileOutputStream("src/main/resources/JSON/" + fileName);
     outputStream.write(json.getBytes());
     outputStream.close();
 
---
+++ src/test/java/edu/tudelft/games/f1manager/core/DriverListTest.java
@@ -0,0 +1,54 @@
+package edu.tudelft.games.f1manager.core;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+
+public class DriverListTest {
+
+  Driver driver1;
+  Driver driver2;
+  PlayerTeam team;
+  ArrayList<Driver> driverArrayList;
+  DriverList driverList;
+
+
+  @Before
+  public void setUp() throws Exception {
+    team = new PlayerTeam(new ArrayList<Driver>(),new ArrayList<Car>(),
+      mock(Strategist.class), mock(Aerodynamicist.class), mock(Mechanic.class),
+      100, true);
+
+    driver1 = new Driver("test testson", team);
+    driver2 = new Driver("Tester", team);
+    driverArrayList = new ArrayList<Driver>();
+    driverArrayList.add(driver1);
+    driverArrayList.add(driver2);
+
+
+    driverList = new DriverList(driverArrayList);
+
+  }
+
+
+  @Test
+  public void updatejsontest() throws IOException {
+
+    driverList.updateJSON();
+
+  }
+
+  @Test
+  public void getjsontest() throws IOException {
+
+    driverList.getJSON();
+
+  }
+
+}
