55ae12ce50a4b6b7392ba18eea81efe3dae60f0e
e_lar <elarb@users.noreply.github.com>
2017-01-07 15:23:49 +0100

    removed template comment

--- src/test/java/edu/tudelft/games/f1manager/core/CarTest.java
+++ src/test/java/edu/tudelft/games/f1manager/core/CarTest.java
@@ -3,9 +3,7 @@ package edu.tudelft.games.f1manager.core;
 import org.junit.After;
 import org.junit.Before;
 
-/**
- * Created by Elias on 30-Nov-16.
- */
+
 public class CarTest {
   @Before
   public void setUp() throws Exception {
