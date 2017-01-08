2897f7b21a13d8907e15c80482becb5836d37351
Wouter <Wouter_van_Lil@live.nl>
2017-01-08 14:01:26 +0100

    Before a new race is launched, AiTeamList.balanceDrivers() should be called, Methods added for checking if an AITeam has less than 2 drivers, and then buys one from a team that has 3 or more, since very driver NEEDS a team

--- src/main/java/edu/tudelft/games/f1manager/core/AiTeamList.java
+++ src/main/java/edu/tudelft/games/f1manager/core/AiTeamList.java
@@ -18,6 +18,33 @@ public class AiTeamList {
   public AiTeamList() {
   }
 
+  public void balanceDrivers() {
+	  for (int i = 0; i < this.aiTeamList.size(); i++) {
+		  if (aiTeamList.get(i).getDriverList().size() < 2) {
+			  if (aiTeamList.get(i).getDriverList().size() < 1) {
+				  this.buyLeftoverDriver(aiTeamList.get(i));
+			  }
+			  this.buyLeftoverDriver(aiTeamList.get(i));
+		  }
+	  }
+  }
+  
+  public void buyLeftoverDriver(AiTeam aiTeam){
+	  boolean bought = false;
+	  for (int i = 0; i < this.aiTeamList.size(); i++) {
+		  if (this.aiTeamList.get(i).getDriverList().size() > 2 && bought == false) {
+			  aiTeam.buyDriver(this.aiTeamList.get(i).getDriverList().get(0));
+			  bought = true;
+		  }
+		  
+		  if (bought == false) {
+			  aiTeam.buyDriver("REFEREER NAAR PLAYERTEAM".getDriverList().get(0));
+			  bought = true;
+		  }
+		  
+	  }
+  }
+  
   public ArrayList<AiTeam> getAiTeamList() {
     return aiTeamList;
   }
