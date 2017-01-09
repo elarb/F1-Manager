98d46b0f5f0c02ccbc9abbbfc1892fc91906cee7
Elias <elarb@users.noreply.github.com>
2017-01-09 17:05:41 +0100

    updated write and read methods, added racelist, added method getPathByRaceName

--- src/main/java/edu/tudelft/games/f1manager/game/Season.java
+++ src/main/java/edu/tudelft/games/f1manager/game/Season.java
@@ -6,7 +6,6 @@ import com.google.gson.GsonBuilder;
 import edu.tudelft.games.f1manager.core.Team;
 
 import java.io.*;
-import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 
 
@@ -16,25 +15,82 @@ public class Season {
    * The current race in the season.
    */
   private int currentRace;
-  private Gson gson = new GsonBuilder()
-    .excludeFieldsWithModifiers(Modifier.FINAL, Modifier.TRANSIENT, Modifier.STATIC)
-    .serializeNulls()
-    .create();
 
   /**
-   * List of drivers sorted by points.
+   * A list of races.
+   */
+  private ArrayList<Race> races;
+
+
+  /**
+   * List of teams sorted by points.
    */
   private ArrayList<Team> constructorStandings;
 
   /**
    * Creates an object that represents a F1 season.
    *
-   * @param currentRace          current race in the season
-   * @param constructorStandings list of drivers sorted by points
+   * @param currentRace current race in the season
+   * @param races       the races in a season
+   * @param teams       list of teams
    */
-  public Season(int currentRace, ArrayList<Team> constructorStandings) {
+  public Season(int currentRace, ArrayList<Race> races, ArrayList<Team> teams) {
     this.currentRace = currentRace;
-    this.constructorStandings = constructorStandings;
+    this.races = races;
+    this.constructorStandings = teams;
+  }
+
+  /**
+   * Returns the SVG Path of a circuit in the form of a String.
+   *
+   * @param raceName name of the circuit
+   * @return the SVG Path of the circuit
+   */
+  public String getPathByRaceName(String raceName) {
+
+    String res = null;
+    for (Race race : this.getRaces()) {
+      if (race.getName().equals(raceName)) {
+        res = race.getCircuit().getPath();
+      }
+    }
+    return res;
+  }
+
+  /**
+   * Reads in season.json returns a season
+   * object if the file is in the appropriate format.
+   *
+   * @return a season
+   */
+  public static Season read() {
+    String filename = "season.json";
+    ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+    InputStream is = classloader.getResourceAsStream(filename);
+    Reader reader = new InputStreamReader(is);
+    Gson gson = new GsonBuilder().create();
+    Season season = gson.fromJson(reader, Season.class);
+
+    return season;
+
+  }
+
+
+  /**
+   * Write the season to season.json.
+   *
+   * @param season the season that gets written
+   * @throws IOException when the file doesn't exist
+   */
+  public static void write(Season season) throws IOException {
+    String filename = "season.json";
+    Gson gson = new GsonBuilder().create();
+    String json = gson.toJson(season);
+
+    FileOutputStream outputStream = new FileOutputStream("src/main/resources/JSON/" + filename);
+    outputStream.write(json.getBytes());
+    outputStream.close();
+    System.out.println("This has been written to season.json : " + json);
   }
 
   public int getCurrentRace() {
@@ -45,61 +101,20 @@ public class Season {
     this.currentRace = currentRace;
   }
 
+  public ArrayList<Race> getRaces() {
+    return races;
+  }
+
+  public void setRaces(ArrayList<Race> races) {
+    this.races = races;
+  }
+
   public ArrayList<Team> getConstructorStandings() {
     return constructorStandings;
   }
 
   public void setConstructorStandings(ArrayList<Team> constructorStandings) {
     this.constructorStandings = constructorStandings;
-  }
-
-
-  /**
-   * Reads a season from "season.json".
-   *
-   * @return a season
-   */
-  public Season read() {
-
-    String fileName = "season.json";
-
-    ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-    InputStream is = classloader.getResourceAsStream("JSON/" + fileName);
-    Reader reader = new InputStreamReader(is);
-
-    return gson.fromJson(reader, Season.class);
-
-  }
-
-  /**
-   * Uses read() to initialize a season object.
-   */
-  public void getJson() {
-
-    Season newseason = read();
-    this.constructorStandings = newseason.getConstructorStandings();
-    this.currentRace = newseason.getCurrentRace();
-
-
-  }
-
-  /**
-   * Updates the "season.json" file with the changed fields
-   *
-   * @throws IOException throws an IO Exception
-   */
-  public void updateJson() throws IOException {
-
-    String fileName = "season.json";
-
-    Season season = new Season(this.getCurrentRace(), this.getConstructorStandings());
-
-    String json = gson.toJson(season);
-
-    FileOutputStream outputStream = new FileOutputStream("src/main/resources/JSON/" + fileName);
-    outputStream.write(json.getBytes());
-    outputStream.close();
-
   }
 
 }
