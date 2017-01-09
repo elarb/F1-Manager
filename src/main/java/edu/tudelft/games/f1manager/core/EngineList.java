8d46f107f04e9a8f61e71b71be5d8f57c66acd8a
Elias <elarb@users.noreply.github.com>
2017-01-09 17:21:31 +0100

    added write and read methods

--- src/main/java/edu/tudelft/games/f1manager/core/EngineList.java
+++ src/main/java/edu/tudelft/games/f1manager/core/EngineList.java
@@ -1,5 +1,9 @@
 package edu.tudelft.games.f1manager.core;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
 import java.util.ArrayList;
 
 public class EngineList {
@@ -11,6 +15,42 @@ public class EngineList {
    */
   public EngineList() {
     this.enginelist = new ArrayList<>();
+  }
+
+  /**
+   * Reads in drivers.json returns a enginelist
+   * object if the file is in the appropriate format.
+   *
+   * @return a enginelist
+   */
+  public static EngineList read() {
+    String filename = "engines.json";
+    ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+    InputStream is = classloader.getResourceAsStream(filename);
+    Reader reader = new InputStreamReader(is);
+    Gson gson = new GsonBuilder().create();
+    EngineList enginelist = gson.fromJson(reader, EngineList.class);
+
+    return enginelist;
+
+  }
+
+
+  /**
+   * Write the enginelist to engines.json.
+   *
+   * @param enginelist the enginelist that gets written
+   * @throws IOException when the file doesn't exist
+   */
+  public static void write(EngineList enginelist) throws IOException {
+    String filename = "engines.json";
+    Gson gson = new GsonBuilder().create();
+    String json = gson.toJson(enginelist);
+
+    FileOutputStream outputStream = new FileOutputStream("src/main/resources/JSON/" + filename);
+    outputStream.write(json.getBytes());
+    outputStream.close();
+    System.out.println("This has been written to the file: " + json);
   }
 
   public void addEngine(Engine engine) {
