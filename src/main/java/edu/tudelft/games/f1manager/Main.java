e3bc73bc8362519a391edef522b285fd910ac6a3
David-Happel <davidvhappel1@gmail.com>
2017-01-10 11:06:52 +0100

    added main fxml to append screens onto and added main menu with exit game functionality

--- src/main/java/edu/tudelft/games/f1manager/Main.java
+++ src/main/java/edu/tudelft/games/f1manager/Main.java
@@ -1,10 +1,12 @@
 package edu.tudelft.games.f1manager;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -17,17 +19,17 @@ public class Main extends Application {
     Font.loadFont(getClass().getClassLoader()
         .getResource("fonts/FuturaLT-Bold.ttf").toExternalForm(), 10);
 
-    Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/Client.fxml"));
+    Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/main.fxml"));
     primaryStage.getIcons().addAll(new Image("img/F1_logo.png"));
     primaryStage.setTitle("F1 Manager");
     primaryStage.setScene(new Scene(root, 1280, 800));
     primaryStage.setResizable(false);
     primaryStage.show();
 
+
   }
 
 
-  public static void main(String[] args) {
-    launch(args);
+  public static void main(String[] args) {launch(args);
   }
 }
