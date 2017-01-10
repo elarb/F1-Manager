5d3476c964deed68d1ddbd556e33df2f3ea7ee16
David-Happel <davidvhappel1@gmail.com>
2017-01-10 15:55:43 +0100

    fixed checkstyle

--- src/main/java/edu/tudelft/games/f1manager/ClientController.java
+++ src/main/java/edu/tudelft/games/f1manager/ClientController.java
@@ -1,49 +1,22 @@
 package edu.tudelft.games.f1manager;
 
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
 public class ClientController {
 
-  @FXML
-  private Pane content;
 
   @FXML
-  private ListView<String> buyableDrivers;
-
-  @FXML
-  private AnchorPane mainscreen;
-
-  @FXML
-  public void handleButtonClick_Configuration() throws IOException {
-    System.out.println("Click! configuration");
-    content.getChildren().clear();
-    content.getChildren().add(FXMLLoader.load(getClass().getResource("/fxml/Configuration.fxml")));
-  }
+  private AnchorPane mainScreen;
 
   public void handleButtonClick_StartGame() throws IOException {
-    mainscreen.getChildren().clear();
-    mainscreen.getChildren().add(FXMLLoader.load(getClass().getResource("/fxml/main menu.fxml")));
+    mainScreen.getChildren().clear();
+    mainScreen.getChildren().add(FXMLLoader.load(getClass().getResource("/fxml/main menu.fxml")));
   }
 
-  public void fillBuyableDrivers(){
-
-    ObservableList<String> data = FXCollections.observableArrayList("driver drive", "max verstappen", "bob bobson");
-    buyableDrivers.setItems(data);
-  }
-
-  public void buyDriver(){
-    String driverName = buyableDrivers.getSelectionModel().getSelectedItem();
-    System.out.println(driverName);
-  }
 
   public void handleButtonClick_newGame() throws IOException{
 
@@ -53,7 +26,7 @@ public class ClientController {
 
   }
 
-  public void handleButtonClick_ExitGame() throws IOException{
+  public void handleButtonClick_ExitGame() throws IOException {
     System.exit(0);
   }
 }
