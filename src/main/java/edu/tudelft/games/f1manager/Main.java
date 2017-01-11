60b5a1a3b311c1207f77da79e0264cdf6fea2e10
David-Happel <davidvhappel1@gmail.com>
2017-01-11 15:58:20 +0100

    fixed more checkstyle

--- src/main/java/edu/tudelft/games/f1manager/Main.java
+++ src/main/java/edu/tudelft/games/f1manager/Main.java
@@ -1,12 +1,10 @@
 package edu.tudelft.games.f1manager;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -35,7 +33,8 @@ public class Main extends Application {
   }
 
 
-  public static void main(String[] args) {launch(args);
+  public static void main(String[] args) {
+    launch(args);
   }
 
 }
