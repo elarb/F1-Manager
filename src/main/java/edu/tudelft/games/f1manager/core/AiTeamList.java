5dd893d2ac9a58c89e3087aac83769e1fab18651
e_lar <elarb@users.noreply.github.com>
2017-01-11 19:50:44 +0100

    removed typetoken, changed to arraylist instead of list

--- src/main/java/edu/tudelft/games/f1manager/core/AiTeamList.java
+++ src/main/java/edu/tudelft/games/f1manager/core/AiTeamList.java
@@ -2,15 +2,10 @@ package edu.tudelft.games.f1manager.core;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
-import org.apache.commons.collections.list.FixedSizeList;
 
 import java.io.*;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Class that represents a list of Computer Teams.
@@ -18,13 +13,13 @@ import java.util.List;
 public class AiTeamList {
 
   private static Gson gson = new GsonBuilder()
-      .excludeFieldsWithModifiers(Modifier.FINAL, Modifier.TRANSIENT, Modifier.STATIC)
-      .serializeNulls().create();
+    .excludeFieldsWithModifiers(Modifier.FINAL, Modifier.TRANSIENT, Modifier.STATIC)
+    .serializeNulls().create();
 
   /**
    * A list of Computer Teams.
    */
-  private List<AiTeam> teams;
+  private ArrayList<AiTeam> teams;
 
 
   /**
@@ -32,7 +27,16 @@ public class AiTeamList {
    * Has a fixed size of 10
    */
   public AiTeamList() {
-    this.teams = FixedSizeList.decorate(Arrays.asList(new AiTeam[10]));
+    this.teams = new ArrayList<>();
+  }
+
+  /**
+   * Adds an aiteam to the list of aiteams.
+   *
+   * @param team the aiteam that gets added
+   */
+  public void add(AiTeam team) {
+    this.getTeams().add(team);
   }
 
   /**
@@ -46,12 +50,8 @@ public class AiTeamList {
     ClassLoader classloader = Thread.currentThread().getContextClassLoader();
     InputStream is = classloader.getSystemClassLoader().getResourceAsStream("JSON/" + filename);
     Reader reader = new InputStreamReader(is);
-    ArrayList<AiTeam> aiTeamArrayList = gson.fromJson(reader, new TypeToken<ArrayList<AiTeam>>() {
-    }.getType());
-    AiTeamList aiteamlist = new AiTeamList();
-    aiteamlist.setTeams(aiTeamArrayList);
 
-    return aiteamlist;
+    return gson.fromJson(reader, AiTeamList.class);
 
   }
 
@@ -63,7 +63,7 @@ public class AiTeamList {
    */
   public void write(String filename) throws IOException {
 
-    String json = gson.toJson(this.teams);
+    String json = gson.toJson(this);
 
     FileOutputStream outputStream = new FileOutputStream("src/main/resources/JSON/" + filename);
     outputStream.write(json.getBytes());
@@ -73,11 +73,26 @@ public class AiTeamList {
     System.out.println(json);
   }
 
-  public List getTeams() {
+  /**
+   * Returnw an AiTeam from the list of teams if exists one having an Id matching the id provided.
+   *
+   * @param id the id that gets compared
+   * @return an AiTeam if there exists one having the id, else null
+   */
+  public AiTeam getAiTeamById(int id) {
+    for (AiTeam team : this.getTeams()) {
+      if (team.getId() == id) {
+        return team;
+      }
+    }
+    return null;
+  }
+
+  public ArrayList<AiTeam> getTeams() {
     return teams;
   }
 
-  public void setTeams(List teams) {
+  public void setTeams(ArrayList<AiTeam> teams) {
     this.teams = teams;
   }
 }
