f90436b7610b2594bc47c39672180673bf100ad1
e_lar <elarb@users.noreply.github.com>
2017-01-11 19:55:13 +0100

    Updated the read method

--- src/main/java/edu/tudelft/games/f1manager/game/Season.java
+++ src/main/java/edu/tudelft/games/f1manager/game/Season.java
@@ -3,11 +3,9 @@ package edu.tudelft.games.f1manager.game;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import edu.tudelft.games.f1manager.core.Team;
-
-import javax.print.DocFlavor;
 import java.io.*;
 import java.lang.reflect.Modifier;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 
 
@@ -31,7 +29,7 @@ public class Season {
   /**
    * List of teams sorted by points.
    */
-  private ArrayList<Team> constructorStandings;
+//  private AbstractMap.SimpleEntry<Integer, Integer> constructorStandings;
 
   /**
    * Creates an object that represents a F1 season.
@@ -40,10 +38,10 @@ public class Season {
    * @param races       the races in a season
    * @param teams       list of teams
    */
-  public Season(int currentRace, ArrayList<Race> races, ArrayList<Team> teams) {
+  public Season(int currentRace, ArrayList<Race> races) {
     this.currentRace = currentRace;
     this.races = races;
-    this.constructorStandings = teams;
+//    this.constructorStandings = teams;
   }
 
   /**
@@ -64,60 +62,17 @@ public class Season {
   }
 
   /**
+   * Adds a race to the list of races of the current season
+   *
+   * @param race the race that gets added
+   */
+  public void addRace(Race race) {
+    this.races.add(race);
+  }
+
+  /**
    * Reads in season.json returns a season
    * object if the file is in the appropriate format.
-   *
-   * @return a season
-   */
-  public static Season read(String filename) {
-//    String filename = "season.json";
-    ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-    InputStream is = classloader.getResourceAsStream("JSON/" + filename);
-    Reader reader = new InputStreamReader(is);
-    Gson gson = new GsonBuilder().create();
-    Season season = gson.fromJson(reader, Season.class);
-
-    return season;
-
-  }
-
-
-  /**
-   * Write the season to season.json.
-   *
-   * @param season the season that gets written
-   * @throws IOException when the file doesn't exist
-   */
-  public static void write(Season season, String filename) throws IOException {
-//    String filename = "season.json";
-    Gson gson = new GsonBuilder().create();
-    String json = gson.toJson(season);
-
-    FileOutputStream outputStream = new FileOutputStream("src/main/resources/JSON/" + filename);
-    outputStream.write(json.getBytes());
-    outputStream.close();
-    System.out.println("This has been written to season.json : " + json);
-  }
-
-  public int getCurrentRace() {
-    return currentRace;
-  }
-
-  public void setCurrentRace(int currentRace) {
-    this.currentRace = currentRace;
-  }
-
-  public ArrayList<Team> getConstructorStandings() {
-    return constructorStandings;
-  }
-
-  public void setConstructorStandings(ArrayList<Team> constructorStandings) {
-    this.constructorStandings = constructorStandings;
-  }
-
-
-  /**
-   * Reads a season from "season.json".
    *
    * @return a season
    */
@@ -139,9 +94,9 @@ public class Season {
    */
   public void write(String filename) throws IOException {
 
-    Season season = new Season(this.getCurrentRace(), this.getConstructorStandings());
+//    Season season = new Season(this.getCurrentRace(), this.getConstructorStandings());
 
-    String json = gson.toJson(season);
+    String json = gson.toJson(this);
 
     FileOutputStream outputStream = new FileOutputStream("src/main/resources/JSON/" + filename);
     outputStream.write(json.getBytes());
@@ -151,5 +106,22 @@ public class Season {
     System.out.println(json);
 
   }
+
+  public int getCurrentRace() {
+    return currentRace;
+  }
+
+  public void setCurrentRace(int currentRace) {
+    this.currentRace = currentRace;
+  }
+
+  public ArrayList<Race> getRaces() {
+    return races;
+  }
+
+  public void setRaces(ArrayList<Race> races) {
+    this.races = races;
+  }
+
 
 }
