0bc9de71c8c4101c123be196e075cb6eb850e285
David-Happel <davidvhappel1@gmail.com>
2017-01-12 19:51:41 +0100

    game loads when pressing new/load game

--- src/main/java/edu/tudelft/games/f1manager/ClientController.java
+++ src/main/java/edu/tudelft/games/f1manager/ClientController.java
@@ -1,7 +1,9 @@
 package edu.tudelft.games.f1manager;
 
+import edu.tudelft.games.f1manager.core.Game;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -23,21 +25,36 @@ public class ClientController {
   }
 
   public void handleButtonClick_newGame() throws IOException {
+    Game game = Game.newgame();
 
+    if (game != null) {
+      gotoClientUi();
+    }
   }
+
+  @FXML
+  private TextField loadgameText;
 
   /**method that is called by the load game button.
    * changes the content of the screen to the main(client.fxml) screen
    * @throws IOException error
    */
   public void handleButtonClick_LoadGame() throws IOException {
+    Game game = Game.loadgame(loadgameText.getText());
+
+    if (game != null) {
+      gotoClientUi();
+    }
+  }
+
+  public void handleButtonClick_ExitGame() {
+    System.exit(0);
+  }
+
+  public void gotoClientUi() throws IOException {
     mainScreen.getChildren().clear();
     FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Client.fxml"));
     loader.setController(this);
     mainScreen.getChildren().add(loader.load());
-  }
-
-  public void handleButtonClick_ExitGame() throws IOException {
-    System.exit(0);
   }
 }
