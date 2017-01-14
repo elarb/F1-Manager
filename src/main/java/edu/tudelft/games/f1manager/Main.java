81af6736754bd9a426308dc064376f1fff72158f
e_lar <elarb@users.noreply.github.com>
2017-01-14 17:00:45 +0100

    refactor

--- src/main/java/edu/tudelft/games/f1manager/Main.java
+++ src/main/java/edu/tudelft/games/f1manager/Main.java
@@ -28,8 +28,6 @@ public class Main extends Application {
     primaryStage.setScene(new Scene(root, 1280, 800));
     primaryStage.setResizable(false);
     primaryStage.show();
-
-
   }
 
   public static void main(String[] args) {
