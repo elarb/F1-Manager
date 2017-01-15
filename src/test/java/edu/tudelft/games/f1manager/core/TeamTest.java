3e419c4861bc047c375fe5b09aff38d27bb80449
timpelser <timpelser@hotmail.fr>
2017-01-15 20:43:28 +0100

    Fixed test issues

--- src/test/java/edu/tudelft/games/f1manager/core/TeamTest.java
+++ src/test/java/edu/tudelft/games/f1manager/core/TeamTest.java
@@ -38,9 +38,6 @@ public class TeamTest {
     Driver driver2 = new Driver("test testy", 1);
     driver2.setValue(64450000);
     playerTeam.addDriver(driver2);
-
-
-
   }
 
 
@@ -73,6 +70,8 @@ public class TeamTest {
   @Test
   public void driver2resultstest() {
 
+
+    System.out.println(playerTeam.getResultsDriver2());
     assertEquals(7.56, playerTeam.getResultsDriver2(), 0.1);
 
   }
