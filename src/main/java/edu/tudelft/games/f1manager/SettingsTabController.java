b2b50f0440e86e334f93d5037fc96557b6c4ff34
PC\david <davidvhappel1@gmail.com>
2017-01-19 18:18:27 +0100

    added loadgame functionality in ui. choose between the existing saved games

--- src/main/java/edu/tudelft/games/f1manager/SettingsTabController.java
+++ src/main/java/edu/tudelft/games/f1manager/SettingsTabController.java
@@ -1,13 +1,14 @@
 package edu.tudelft.games.f1manager;
 
+import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
+import edu.tudelft.games.f1manager.game.Game;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
 
-import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 
 public class SettingsTabController {
@@ -18,7 +19,13 @@ public class SettingsTabController {
   private Pane saveGamePane;
 
   @FXML
-  JFXTextField saveNameField;
+  private JFXTextField saveNameField;
+
+  @FXML
+  private JFXListView loadGameList;
+
+  @FXML
+  private Pane loadGamePane;
 
   public void injectMainController(ClientController clientController) {
     this.clientController = clientController;
@@ -38,5 +45,35 @@ public class SettingsTabController {
     }else{
       saveNameField.setStyle("-fx-background-color: red");
     }
+  }
+
+  public void handleButtonClick_LoadGame(){
+    System.out.println("load");
+    updateLoadGameList();
+    loadGamePane.setVisible(true);
+    loadGamePane.setDisable(false);
+  }
+
+  public void loadGame(){
+    String saveName = (String) loadGameList.getSelectionModel().getSelectedItem();
+
+    if(saveName != null){
+      clientController.setGame(Game.loadgame(saveName));
+      loadGamePane.setDisable(true);
+      loadGamePane.setVisible(false);
+    }
+
+  }
+
+  private void updateLoadGameList(){
+    ObservableList<String> savedGames = FXCollections.observableArrayList();
+
+    File file = new File("src/main/resources/JSON/");
+    String[] saves = file.list((dir, name) -> new File(dir, name).isDirectory());
+
+    for (String name : saves) {
+      savedGames.add(name);
+    }
+    loadGameList.setItems(savedGames);
   }
 }
