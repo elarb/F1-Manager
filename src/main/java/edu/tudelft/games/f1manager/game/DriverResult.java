5c411c6899b44530de3ce5c78a59a67f7de77aa9
timpelser <timpelser@hotmail.fr>
2017-01-22 13:43:43 +0100

    Added if statement to return CRASHED when crashed

--- src/main/java/edu/tudelft/games/f1manager/game/DriverResult.java
+++ src/main/java/edu/tudelft/games/f1manager/game/DriverResult.java
@@ -55,16 +55,25 @@ public class DriverResult {
 
 
 
+
+
     double totalseconds = this.getTime();
+
+    if(totalseconds == 100000000){
+
+      return "CRASHED";
+
+    }
+
     int hours = (int) Math.round(totalseconds / 3600);
     int minutes = (int) Math.round(totalseconds % 3600) / 60;
     int seconds = (int) Math.round(totalseconds % 60);
 
     return hours + " hours " + minutes + " minutes " + seconds + " seconds";
 
-
-
   }
+
+
 
 }
 
