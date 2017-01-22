92eaeec3fb49cde1cace52a2281a61bf3c93f658
e_lar <elarb@users.noreply.github.com>
2017-01-22 11:11:53 +0100

    checkstyle changes

--- src/main/java/edu/tudelft/games/f1manager/core/Mechanic.java
+++ src/main/java/edu/tudelft/games/f1manager/core/Mechanic.java
@@ -30,19 +30,16 @@ public class Mechanic implements Upgradeable {
   /**
    * Improves the pit-stop time of the mechanic.
    *
-   * @param team team the mechanic is in,
-   *             the budget is getting checked to make sure there is enough
    */
-  
   public GameEvent upgrade(int stat) {
-	  if (this.pitstopTime > 2) {
-		    this.pitstopTime -= 1;
-		    updateUpgradePrice();
-		    String msg = "Your Mechanic has been upgraded! New pitstoptime: " + this.pitstopTime;
-		    return new GameEvent(msg, GameEvent.Type.UPGRADE);
-	    }
-	  String msg = "Your pitstoptime is already at its lowest";
-	  return new GameEvent(msg, GameEvent.Type.UPGRADE);
+    if (this.pitstopTime > 2) {
+      this.pitstopTime -= 1;
+      updateUpgradePrice();
+      String msg = "Your Mechanic has been upgraded! New pitstoptime: " + this.pitstopTime;
+      return new GameEvent(msg, GameEvent.Type.UPGRADE);
+    }
+    String msg = "Your pitstoptime is already at its lowest";
+    return new GameEvent(msg, GameEvent.Type.UPGRADE);
   }
 
   public void updateUpgradePrice() {
