4036a9057edd43bebdcb0978adb4a8e50131f63e
PC\david <davidvhappel1@gmail.com>
2017-01-26 12:25:47 +0100

    added more driver info to buydriverlist

--- src/main/java/edu/tudelft/games/f1manager/TableDriver.java
+++ src/main/java/edu/tudelft/games/f1manager/TableDriver.java
@@ -1,16 +1,22 @@
 package edu.tudelft.games.f1manager;
 
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import javafx.beans.property.*;
 
 class TableDriver extends RecursiveTreeObject<TableDriver> {
 
   StringProperty name;
+  DoubleProperty speed;
+  DoubleProperty racecraft;
+  DoubleProperty strategyinsight;
   StringProperty value;
 
-  TableDriver(String name, int value) {
+  TableDriver(String name, double speed, double racecraft, double strategyinsight, int value) {
     this.name = new SimpleStringProperty(name);
+    this.speed = new SimpleDoubleProperty(speed);
+    this.racecraft = new SimpleDoubleProperty(racecraft);
+    this.strategyinsight = new SimpleDoubleProperty(strategyinsight);
+
     String tempValue = "";
     int amount = 0;
     for (char ch: Integer.toString(value).toCharArray()) {
